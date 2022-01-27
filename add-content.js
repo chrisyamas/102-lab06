69675401709caa1624d72b9febca1af42c8523e7
Christopher Yamas <yamas.chris@gmail.com>
2022-01-27 15:34:48 -0600

    html, css, js files created

---
+++ add-content.js
@@ -0,0 +1,11 @@
+// SETTING VARIABLE THROUGH PROMPT
+let firstName = prompt("What's your name?")
+
+// IF TRUE
+if (firstName === 'Alex') {
+    alert('Zork loves you')
+  }
+  // ELSE a.k.a OTHERWISE ->  FALSE
+  else {
+    alert('Imposter')
+  }
